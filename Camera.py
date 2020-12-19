c5d5e200468e9eadf7e4e30d592002e9c06b2057
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-12-19 05:48:35 +0000

    wl18xx: show rx_frames_per_rates as an array as it really is

--- Camera.py
+++ Camera.py
@@ -13,3 +13,21 @@ from threading import Thread
 
 import copy
 
+
+class Camera:
+    def __init__(self, source):
+        # Video capture
+        self.video = cv2.VideoCapture(source)
+        self.soundFile = os.getcwd() + os.sep + 'car-honk.mp3'
+        self.soundCondition = False
+        self.uploadCondition = False
+        pg.mixer.init()
+        pg.mixer.music.load(self.soundFile)
+
+    def getRawFrame(self):
+        # Returns the raw frame
+        _, frameToReturn = self.video.read()
+        return frameToReturn
+
+    # Frame with annotations
+    def getFrameAnnotations(self):
