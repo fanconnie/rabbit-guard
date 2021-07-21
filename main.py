02ab887355748127af751ed805e561b2442c3854
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-07-21 21:52:55 +0000

    ath5k: fix spontaneus AR5312 freezes

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def gen(camera):
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         except Exception as e:
-            exceptionType, exceptionObject, exceptionThrowb
+            exceptionType, exceptionObject, exceptionThrowback = sys.exc_info
