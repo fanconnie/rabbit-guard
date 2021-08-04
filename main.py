58593be9cc82bd585d35f16fdc53ce29bac7b291
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-08-04 21:54:30 +0000

    Bumped version to 2.0.0.

--- main.py
+++ main.py
@@ -25,4 +25,5 @@ def gen(camera):
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         except Exception as e:
             exceptionType, exceptionObject, exceptionThrowback = sys.exc_info()
-            fileName = os.path.split(exceptionThrowback.tb_frame.f_code.co_filen
+            fileName = os.path.split(exceptionThrowback.tb_frame.f_code.co_filename)[1]
+         
